852c11f53c079e8579a9dec0878cd4026b54a937
thomas denimal <thomas.denimal@gmail.com>
2018-09-27 22:34:34 +0200

    babynames done

--- INFMDI721/Lesson1/babynames/babynames.py
+++ INFMDI721/Lesson1/babynames/babynames.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
@@ -35,34 +35,50 @@ Suggested milestones for incremental development:
 """
 
 def extract_names(filename):
-  """
-  Given a file name for baby.html, returns a list starting with the year string
-  followed by the name-rank strings in alphabetical order.
-  ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
-  """
-  # +++your code here+++
-  return
+    """
+    Given a file name for baby.html, returns a list starting with the year string
+    followed by the name-rank strings in alphabetical order.
+    ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
+    """
+    output_list=[]
+    f = open(filename,'r')
+    for line in f:
+        match_y = re.search(r'\<h2 align\=\"center\"\>Popularity in (.*)\<\/h3\>',line)
+        match_baby = re.search(r'\<tr align\=\"right\"\>\<td\>(.*)</td><td>(.*)\<\/td\>\<td\>(.*)\<\/td\>',line)
+
+        if  match_baby:
+            rank=match_baby.group(1)
+            male=match_baby.group(2)
+            female=match_baby.group(3)
+            output_list.append([male,rank])
+            output_list.append([female,rank])
+        elif match_y:
+            year=match_y.group(1)
+    sorted_list=sorted(output_list,key=lambda x:x[0],reverse=False)
+    return sorted_list
 
 
 def main():
   # This command-line parsing code is provided.
   # Make a list of command line arguments, omitting the [0] element
   # which is the script itself.
-  args = sys.argv[1:]
+    args = sys.argv[1:]
 
-  if not args:
-    print 'usage: [--summaryfile] file [file ...]'
-    sys.exit(1)
+    if not args:
+        print('usage: [--summaryfile] file [file ...]')
+        sys.exit(1)
 
   # Notice the summary flag and remove it from args if it is present.
-  summary = False
-  if args[0] == '--summaryfile':
-    summary = True
-    del args[0]
+    summary = False
+    if args[0] == '--summaryfile':
+        summary = True
+        del args[0]
 
-  # +++your code here+++
+    for file in args:
+        print((extract_names(file)))
+
   # For each filename, get the names, then either print the text output
   # or write it to a summary file
-  
+
 if __name__ == '__main__':
   main()
