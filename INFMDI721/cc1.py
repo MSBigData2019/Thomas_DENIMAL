b350282d7cf8b0bc330c0c25d99762410c1cf427
thomas denimal <thomas.denimal@gmail.com>
2018-09-28 14:06:01 +0200

    new2

--- INFMDI721/cc1.py
+++ INFMDI721/cc1.py
@@ -12,8 +12,7 @@ def string_times(string, n):
 # Given an array of ints, return True if one of the first 4 elements
 # in the array is a 9. The array length may be less than 4.
 def array_front9(nums):
-    longueur=min(len(nums) and  4)
-
+    longueur=min([len(nums),4])
 
     for i in nums[:longueur]:
         if (i==9):
